1b6bfb6bdd79a6f1100d8b74ab4b436a0def5fdc
taikurachi <tk2958@nyu.edu>
2025-01-27 13:48:29 -0500

    adding favicon

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Outfit } from "next/font/google";
-
+import Head from "next/head";
 const outfit = Outfit({
   subsets: ["latin"],
   weight: ["400", "600", "700", "800", "900"],
@@ -15,6 +15,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
       <body className={`antialiased ${outfit.className}`}>{children}</body>
     </html>
   );

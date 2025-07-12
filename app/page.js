a9c3792aebdc24683d67dcb3a8d760f6fe296072
taikurachi <tk2958@nyu.edu>
2025-07-11 18:46:03 -0700

    removing songsync

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Footer from "./components/utils/Footer";
 import Header from "./components/utils/Header";
 import Work from "./components/work/Work";
-import Hero from "./components/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* <Hero /> */}
       <Work />
       <Footer />
     </>
